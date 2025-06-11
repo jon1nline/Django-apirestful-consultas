894482e8bb46b622971bb33ebbd6427d93d0a9f0
jon1nline <www.jhonattan@hotmail.com>
2025-06-11 15:35:41 -0300

    settings changes

--- lacreisaude/settings.py
+++ lacreisaude/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'lacreiapp',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',  
+        'HOST': 'localhost',  
         'PORT': 5432,
     }
 }
