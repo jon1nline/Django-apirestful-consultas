9aad257caff721489f0a42921618fff43b7c9bfb
jon1nline <www.jhonattan@hotmail.com>
2025-06-12 16:28:09 -0300

    setting changes

--- lacreisaude/settings.py
+++ lacreisaude/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'yourpassword',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',  
         'PORT': 5432,
     }
