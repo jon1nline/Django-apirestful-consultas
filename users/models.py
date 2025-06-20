7c49a0fa4fe4a20ac468e8a9c4b0f1ecc763ce7d
jon1nline <www.jhonattan@hotmail.com>
2025-06-20 18:17:03 -0300

    model changes

--- users/models.py
+++ users/models.py
@@ -1,16 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O email deve ser fornecido')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 class User(AbstractUser):
+    username = None  
     nome_social = models.CharField(max_length=255)
-    email = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)
-    username = None
+    email = models.EmailField(unique=True)  
+    
+    USERNAME_FIELD = 'email'  
+    REQUIRED_FIELDS = ['nome_social', 'password']  
+    
+    objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] 
-
-
-   
+    def __str__(self):
+        return self.email
 
