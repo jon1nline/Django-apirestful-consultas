f87caca4ae90aac4e291c3b303c01a78d8f9158e
jon1nline <www.jhonattan@hotmail.com>
2025-06-26 16:46:49 -0300

    settings change

--- lacreisaude/settings.py
+++ lacreisaude/settings.py
@@ -83,7 +83,21 @@ WSGI_APPLICATION = "lacreisaude.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+if os.getenv('GITHUB_ACTIONS') == 'true':
+    # GitHub Actions database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+else:
+    # Postgres Local:localhost / docker: db
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
